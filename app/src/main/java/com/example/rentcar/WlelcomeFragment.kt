1ec0dd1e6fa73dec22a5636350623489cbc14eb9
yamlakyam <yamlakyam@gmail.com>
2021-11-29 10:55:45 +0300

    added a dum logic to connect the login fragment with the home fragment

--- app/src/main/java/com/example/rentcar/WlelcomeFragment.kt
+++ app/src/main/java/com/example/rentcar/WlelcomeFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 
 class WlelcomeFragment : Fragment() {
 
@@ -16,6 +19,15 @@ class WlelcomeFragment : Fragment() {
 
         view.findViewById<TextView>(R.id.registerTxt).setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_wlelcomeFragment_to_registrationFragment))
+
+        view.findViewById<Button>(R.id.loginButton).setOnClickListener{
+            val email =view.findViewById<EditText>(R.id.editTextTextEmailAddress2).text.toString()
+            val password =view.findViewById<EditText>(R.id.editTextTextPassword).text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty()){
+                findNavController().navigate(R.id.action_wlelcomeFragment_to_homeFragment)
+            }
+        }
 
         return view
     }
