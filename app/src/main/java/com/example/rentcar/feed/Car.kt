ba33bbd72098f4f081524e2519f8ee6979403668
yamlakyam <yamlakyam@gmail.com>
2021-11-30 11:36:26 +0300

    added a few more items to the list of cars

--- app/src/main/java/com/example/rentcar/feed/Car.kt
+++ app/src/main/java/com/example/rentcar/feed/Car.kt
@@ -31,7 +31,35 @@ val CARS = listOf<Car>(
         location = "Addis Ababa",
         picture = R.drawable.car_3,
         price = "2000ETB"
-    )
+    ),
+    Car(
+        id = 4,
+        name = "ElectricCar",
+        location = "Addis Ababa",
+        picture = R.drawable.car_4,
+        price = "3000ETB"
+    ),
+    Car(
+        id = 5,
+        name = "Small Truck",
+        location = "Addis Ababa",
+        picture = R.drawable.car_5,
+        price = "1500ETB"
+    ),
+    Car(
+        id = 6,
+        name = "Electric Car",
+        location = "Addis Ababa",
+        picture = R.drawable.car_6,
+        price = "3000ETB"
+    ),
+    Car(
+        id = 7,
+        name = "BMW",
+        location = "Addis Ababa",
+        picture = R.drawable.car_7,
+        price = "1500ETB"
+    ),
 
 
 )
